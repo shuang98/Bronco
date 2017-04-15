a6b009095280adc53bcc784621f9ec3d234e6faf
Gary Li <gary.li@berkeley.edu>
2017-04-15 11:50:28 -0700

    added editor

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Bronco
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
   end
 end
 
+
+config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w( ckeditor/* )
+config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
